7b4abfa6aa60f6002f9b0a244a7493b8d30fef28
Sergio Ramirez <sergio@users.noreply.github.com>
2026-01-15 07:28:23 -0500

    Fix Vite alias @ to client/src

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
   root: "client",
   base: "/safeconnect-website/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./client/src", import.meta.url)),
+    },
+  },
 });
